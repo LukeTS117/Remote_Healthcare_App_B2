af692d1918b9561a4646f83f548d7b40c5047241
Luke Taylor <luketaylor001@gmail.com>
2019-09-04 15:55:13 +0200

    Visual studio project added

---
+++ Remote_Healthcare_App_B2/Remote_Healthcare_App_B2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Remote_Healthcare_App_B2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
